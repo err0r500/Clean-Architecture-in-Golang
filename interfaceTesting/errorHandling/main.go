3eb976304ce302b9327d5f922d5f5fb5f74bd56a
Matthieu Jacquot <matth@localhost.localdomain>
2017-07-07 08:46:58 +0200

    improve errors messages

--- interfaceTesting/errorHandling/main.go
+++ interfaceTesting/errorHandling/main.go
@@ -23,18 +23,18 @@ type orderReader interface {
 func CheckOrder(oR orderReader, orderID int) error {
 	order, err := oR.GetOrder(orderID)
 	if err != nil || order == nil {
-		return errors.New("text")
+		return errors.New("unable to retreive the order")
 	}
 	if order.ID != orderID || order.UserID == 0 {
-		return errors.New("text")
+		return errors.New("the order returned is wrong")
 	}
 
 	user, err := oR.GetUser(order.UserID)
 	if err != nil || user == nil {
-		return errors.New("text")
+		return errors.New("unable to retreive the user")
 	}
 	if order.UserID != user.ID {
-		return errors.New("text")
+		return errors.New("the user returned is wrong")
 	}
 	fmt.Printf("Order %d belongs to user %s !\n", order.ID, user.Name)
 	return nil
